489742e16d65af4dd1bdc7e94e3a9f18de4f01f2
saritapel <saritapel@gmail.com>
2017-07-30 14:00:01 +0300

    gallery new by pics staty

--- app/app.js
+++ app/app.js
@@ -1,10 +1,30 @@
-appGCard = angular.module( "appcard", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
+appGCard = angular.module("appcard", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
 
 
 
-appGCard.controller( "cardCtrl", function($scope, $http) {
-    $http.get("data/cards.json").then(function (response) {
-        $scope.cards = response.data;
-    });
+appGCard.controller("galleryCtrl", function ($scope, $http) {
 
-});
+    function GCgallery(galleryObject) {
+        this.cardTitle = galleryObject.cardTitle;
+        this.img = galleryObject.img;
+        this.url = galleryObject.url;
+         this.isSelected = false;
+    }
+
+ // $http.get("data/galleryCards.json").then(function (response) {
+  //     $scope.GCgallery = response.data;
+
+
+ // });
+
+
+ $http.get("data/galleryCards.json").then(function(response) {
+    $scope.GCgallery = [];
+    for (var i = 0; i < response.data.length; i++) {
+      $scope.GCgallery.push(new GCgallery(response.data[i]))
+    }
+  }, function(response) {
+    console.log("Error!!! " + response.statusText);
+  });
+
+ 
