5cb09f15ecefd1b8a9b7ce6e1d0bbb470cb761c1
saritapel <saritapel@gmail.com>
2017-07-31 23:26:43 +0300

    testing pics

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ appGCard = angular.module("appcard", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
 
 appGCard.controller("galleryCtrl", function ($scope, $http) {
 
-    function GCgallery(galleryObject) {
+    function Card(galleryObject) {
         this.cardTitle = galleryObject.cardTitle;
         this.img = galleryObject.img;
         this.url = galleryObject.url;
@@ -18,13 +18,46 @@ appGCard.controller("galleryCtrl", function ($scope, $http) {
  // });
 
 
- $http.get("data/galleryCards.json").then(function(response) {
-    $scope.GCgallery = [];
+ $http.get("card/gallery.json").then(function(response) {
+    $scope.cards = [];
     for (var i = 0; i < response.data.length; i++) {
-      $scope.GCgallery.push(new GCgallery(response.data[i]))
+      $scope.cards.push(new Card(response.data[i]))
     }
   }, function(response) {
     console.log("Error!!! " + response.statusText);
   });
 
- 
+ /////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  $scope.filterBycardTitle = function(cards) {
+
+    if (!$scope.filterText) {
+      return true;
+    } else if (cards.cardTitle.toLowerCase().indexOf($scope.filterText.toLowerCase()) != -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
+  $scope.liClicked = function(cards) {
+    // loop through actors to reset them to false
+    for (var i = 0; i < $scope.cards.length; i++) {
+      $scope.cards[i].isSelected = false;
+    }
+
+    cards.isSelected = true;
+    var arr = document.getElementsByClassName("img-selected");
+    for (var i = 0; i < arr.length; i++) {
+      arr[i].className = "galleryImg";
+    }
+    $event.currentTarget.className = "img-selected";
+  };
+
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
